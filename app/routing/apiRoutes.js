287bf1971f88a5a6d5871985eef1cf48619edabc
ivancho277 <rouskoi@live.com>
2019-04-04 20:30:36 -0700

    added post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,8 @@ module.exports = function(app){
     app.get("/api/data", (req, res) => {
         res.json(friendData);
     })
+    app.post("/api/data", function(req, res) {
+        friendData.push(req.body)
+    })
+
 }
