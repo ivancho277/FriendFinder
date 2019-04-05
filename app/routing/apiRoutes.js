5141049f0f3582f1108bfe8fce4f33631ee9b86a
ivancho277 <rouskoi@live.com>
2019-04-05 07:47:09 -0700

    almost done with findclosest friend algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,14 +10,36 @@ module.exports = function(app){
 
 }
 
-function checkArrayDif(arr){
-    var sumarr = arr.map(x => x.reduce((a,b) => a +b))
-    
-    return sumarr
-    
-  }
+// function checkArrayDif(myAnsArry, arr){
+//     var sumarr = arr.map(x => x.reduce((a,b) => a +b))
+//     var mysum = myAnsArry.reduce((a,b) => a+b)
+//     //check what my sum is closest to
+//     var closest = sumarr[0]
+//     //array to hold multiple values if they are closest;
+//     var matches = []
+//     for(var i = 0; i < sumarr.length; i++){
+//       if(Math.abs(sumarr[i] - mysum) === 0){
+//         matches.push(sumarr[i])
+//       }
+//       else{
+//         if(Math.abs(sumarr[i] - mysum) < closest){
+//           closest = sumarr[i]
+//         }
+//       }
+//     }
+//     console.log(mysum)
+//     return closest
+//   }
   
-  var arr1 = [5,3,3,1,4,4,5,4,1,5]
-  var arr2 = [2,2,1,1,5,5,5,5,5,5]
-  var arr3 = [4,1,1,2,3,1,4,5,5,1]
-  var arrofarr = [arr1, arr3, arr2]
+  
+//   var myarr = [1,2,3,4,5,5,4,3,2,1]
+//   var arr1 = [5,3,3,1,4,4,5,4,1,5]
+//   var arr2 = [2,2,1,1,5,5,5,5,5,5]
+//   var arr3 = [4,1,1,2,3,1,4,5,5,1]
+//   var arrofarr = [arr1, arr3, arr2]
+  
+  
+  
+  
+//   console.log(checkArrayDif(myarr, arrofarr))
+//   console.log(myarr)
